eb40eb6b42792cbc1df5b767f602412f15ba16c8
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2018-12-04 14:47:59 +0100

    Add java tests

--- slang-tester/src/main/java/SlangTester.java
+++ slang-tester/src/main/java/SlangTester.java
@@ -3,13 +3,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.sonarsource.scala.converter.ScalaConverter;
+import org.sonarsource.sjava.converter.SJavaConverter;
 import org.sonarsource.slang.api.ASTConverter;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class SlangTester {
 
-  private static final Path fileName = Paths.get("slang-tester\\src\\main\\resources\\test1.scala");
+//  private static final Path fileName = Paths.get("slang-tester\\src\\main\\resources\\test1.scala");
+
+  private static final Path fileName = Paths.get("slang-tester\\src\\main\\resources\\test1.java");
+
 
   public static void main(String[] args) {
 
@@ -20,12 +24,18 @@ public class SlangTester {
 //    LanguageTester kotlinTester = new LanguageTester(new KotlinConverter());
 //    LanguageTester rubyTester = new LanguageTester(new RubyConverter());
 
-    LanguageTester scalaTester = new LanguageTester(new ScalaConverter());
+//    LanguageTester scalaTester = new LanguageTester(new ScalaConverter());
+
+    LanguageTester javaTester = new LanguageTester(new SJavaConverter());
+
+    System.out.println(javaTester.getTable(content));
+
+    System.out.println(javaTester.getDotCfg(content));
 
 
-    System.out.println(scalaTester.getTable(content));
-
-    System.out.println(scalaTester.getDotCfg(content));
+//    System.out.println(scalaTester.getTable(content));
+//
+//    System.out.println(scalaTester.getDotCfg(content));
   }
 
 
