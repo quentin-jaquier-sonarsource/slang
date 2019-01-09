d656d867c088b0a195aa0f7a56bd0afd092dcb9c
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2019-01-09 14:10:25 +0100

    Add backward analysis, (null == p), p != null

--- slang-checks/src/main/java/org/sonarsource/slang/checks/NullDereferenceBeliefStyleCheck.java
+++ slang-checks/src/main/java/org/sonarsource/slang/checks/NullDereferenceBeliefStyleCheck.java
@@ -60,7 +60,10 @@ public class NullDereferenceBeliefStyleCheck implements SlangCheck {
     init.register(FunctionDeclarationTree.class, (ctx, functionDeclarationTree) -> {
       if(functionDeclarationTree.body() != null){
         ControlFlowGraph cfg = ControlFlowGraph.build(functionDeclarationTree, true);
-        NullTracking nullTracking = NullTracking.analyse(cfg);
+
+        NullTrackingForward nullTrackingForward = NullTrackingForward.analyse(cfg);
+        NullTrackingBackward nullTrackingBackward = NullTrackingBackward.analyse(cfg);
+
 
         Set<String> localIdentifier =
             functionDeclarationTree.descendants()
@@ -81,7 +84,8 @@ public class NullDereferenceBeliefStyleCheck implements SlangCheck {
         localIdentifier.addAll(parameters);
 
         for (CfgBlock block : cfg.blocks()) {
-          block.elements().forEach(element -> checkElement(element, nullTracking.getOut(block), ctx, cfg, localIdentifier));
+          Set<String> unionOut = Sets.union(nullTrackingForward.getOut(block), nullTrackingBackward.getOut(block));
+          block.elements().forEach(element -> checkElement(element, unionOut, ctx, cfg, localIdentifier));
         }
       }
     });
@@ -90,43 +94,39 @@ public class NullDereferenceBeliefStyleCheck implements SlangCheck {
   private void checkElement(Tree element, Set<String> out, CheckContext ctx, ControlFlowGraph cfg, Set<String> localIdentifier) {
     if(element instanceof BinaryExpressionTree){
       BinaryExpressionTree binOp = (BinaryExpressionTree) element;
-      if(binOp.operator().equals(BinaryExpressionTree.Operator.EQUAL_TO)){
+      if(binOp.operator().equals(BinaryExpressionTree.Operator.EQUAL_TO) || binOp.operator().equals(BinaryExpressionTree.Operator.NOT_EQUAL_TO)){
         processEqualTo(binOp, out, ctx, cfg, localIdentifier);
       }
     }
   }
 
   private void processEqualTo(BinaryExpressionTree element, Set<String> out, CheckContext ctx, ControlFlowGraph cfg, Set<String> localIdentifier) {
+
+    IdentifierTree pointerCheckedForNull = null;
     if(element.rightOperand() instanceof LiteralTree) {
-      IdentifierTree lhs = NullTracking.getIdentifierIfPresent(element.leftOperand());
-      if(lhs == null){
-        return;
-      }
-
       LiteralTree rhs = (LiteralTree) element.rightOperand();
-
-      String pointerChecked;
-
-      //TODO: Support null == p correctly
-      if(rhs.value().equals("null")){
-        pointerChecked = lhs.name();
-      } else if(lhs.name().equals("null")){
-        pointerChecked = rhs.value();
-      } else {
-        return;
+      if(rhs.value().equals("null")) {
+        pointerCheckedForNull = NullTrackingForward.getIdentifierIfPresent(element.leftOperand());
       }
+    } else if(element.leftOperand() instanceof LiteralTree) {
+      LiteralTree lhs = (LiteralTree) element.leftOperand();
+      if(lhs.value().equals("null")) {
+        pointerCheckedForNull = NullTrackingForward.getIdentifierIfPresent(element.rightOperand());
+      }
+    }
 
-      if(out.contains(pointerChecked) && localIdentifier.contains(pointerChecked)) {
+    if(pointerCheckedForNull != null){
+      if(out.contains(pointerCheckedForNull.name()) && localIdentifier.contains(pointerCheckedForNull.name())) {
         ctx.reportIssue(element, "This check is either always false, or a null pointer has been raised before.");
       }
     }
   }
 
-  private static class NullTracking {
+  private static class NullTrackingForward {
     private final ControlFlowGraph cfg;
     private final Map<CfgBlock, Set<String>> out = new HashMap<>();
 
-    private NullTracking(ControlFlowGraph cfg) {
+    private NullTrackingForward(ControlFlowGraph cfg) {
       this.cfg = cfg;
     }
 
@@ -134,8 +134,8 @@ public class NullDereferenceBeliefStyleCheck implements SlangCheck {
       return out.get(block);
     }
 
-    private static NullTracking analyse(ControlFlowGraph cfg) {
-      NullTracking nullTracking = new NullTracking(cfg);
+    private static NullTrackingForward analyse(ControlFlowGraph cfg) {
+      NullTrackingForward nullTracking = new NullTrackingForward(cfg);
       //Generate kill/gen for each block in isolation
       Map<CfgBlock, Set<String>> kill = new HashMap<>();
       Map<CfgBlock, Set<String>> gen = new HashMap<>();
@@ -214,6 +214,142 @@ public class NullDereferenceBeliefStyleCheck implements SlangCheck {
       blockGen.removeAll(shortCircuited);
     }
     
+    private void processVariable(IdentifierTree element, Set<String> blockKill, Set<String> blockGen) {
+      blockKill.add(element.name());
+      blockGen.remove(element.identifier());
+    }
+
+    private void processMemberSelect(MemberSelect element, Set<String> blockGen) {
+      processPointerUse(element.expression(), blockGen);
+    }
+
+    private void processPointerUse(Tree element, Set<String> blockGen) {
+      IdentifierTree id = getIdentifierIfPresent(element);
+      if(id != null) {
+        String name = id.name();
+        if(isLocal()) {
+          blockGen.add(name);
+        }
+      }
+    }
+
+    private void processAssignment(AssignmentExpressionTree element, Set<String> blockKill, Set<String> blockGen) {
+      IdentifierTree lhs = getIdentifierIfPresent(element.leftHandSide());
+      if (lhs != null) {
+        String name = lhs.name();
+        if(isLocal()) {
+          //if we see an assignment, we remove all previously used  pointer (we don't know anything for them anymore)
+          blockKill.add(name);
+          blockGen.remove(name);
+        }
+      }
+    }
+
+    private boolean isLocal() {
+      return true; //TODO
+    }
+
+    private static IdentifierTree getIdentifierIfPresent(Tree tree){
+      if(tree instanceof IdentifierTree) {
+        return (IdentifierTree) tree;
+      }
+      return null;
+    }
+  }
+
+
+  private static class NullTrackingBackward {
+    private final ControlFlowGraph cfg;
+    private final Map<CfgBlock, Set<String>> out = new HashMap<>();
+
+    private NullTrackingBackward(ControlFlowGraph cfg) {
+      this.cfg = cfg;
+    }
+
+    private Set<String> getOut(CfgBlock block) {
+      return out.get(block);
+    }
+
+    private static NullTrackingBackward analyse(ControlFlowGraph cfg) {
+      NullTrackingBackward nullTracking = new NullTrackingBackward(cfg);
+      //Generate kill/gen for each block in isolation
+      Map<CfgBlock, Set<String>> kill = new HashMap<>();
+      Map<CfgBlock, Set<String>> gen = new HashMap<>();
+
+      for (CfgBlock block: cfg.blocks()) {
+        Set<String> blockKill = new HashSet<>();
+        Set<String> blockGen = new HashSet<>();
+
+        nullTracking.processBlockElements(block, blockKill, blockGen);
+
+        kill.put(block, blockKill);
+        gen.put(block, blockGen);
+      }
+
+      nullTracking.analyzeCFG(kill, gen);
+
+      // Make things immutable.
+      for (Map.Entry<CfgBlock, Set<String>> blockSetEntry : nullTracking.out.entrySet()) {
+        blockSetEntry.setValue(ImmutableSet.copyOf(blockSetEntry.getValue()));
+      }
+
+      return nullTracking;
+    }
+
+    //Forward analysis
+    private void analyzeCFG(Map<CfgBlock, Set<String>> kill, Map<CfgBlock, Set<String>> gen) {
+      Deque<CfgBlock> workList = new LinkedList<>();
+      workList.addAll(Lists.reverse(cfg.blocks()));
+      while (!workList.isEmpty()) {
+        CfgBlock  block = workList.removeFirst();
+
+        Set<String> blockIn;
+
+        //Collect all successor out set
+        List<Set<String>> preds = block.successors().stream().map(out::get).filter(Objects::nonNull).collect(Collectors.toList());
+
+        if(!preds.isEmpty()){
+          Set<String> newBlockIn = new HashSet<>(preds.get(0));
+          for (int i = 1; i < preds.size(); i++){
+            newBlockIn = Sets.intersection(newBlockIn, preds.get(i));
+          }
+          blockIn = new HashSet<>(newBlockIn);
+        } else {
+          blockIn = new HashSet<>();
+        }
+
+        // out = gen and (in - kill)
+        Set<String> newOut = new HashSet<>(gen.get(block));
+        newOut.addAll(Sets.difference(blockIn, kill.get(block)));
+
+        if (newOut.equals(out.get(block))) {
+          continue;
+        }
+        out.put(block, newOut);
+        block.predecessors().forEach(workList::addLast);
+      }
+    }
+
+    private void processBlockElements(CfgBlock block, Set<String> blockKill, Set<String> blockGen) {
+      // process elements from bottom to top: order matter because we are going to remove pointer
+      // from gen block (in assign and declaration)
+      Set<String> shortCircuited = new HashSet<>();
+      for (Tree element : Lists.reverse(block.elements())) {
+        if(element instanceof AssignmentExpressionTree){
+          processAssignment((AssignmentExpressionTree) element, blockKill, blockGen);
+        } else if(element instanceof MemberSelect){
+          //Gen only in reliable block (not in native)
+          if(block.isReliable()) {
+            processMemberSelect((MemberSelect) element, blockGen);
+          }
+        } else if(element instanceof VariableDeclarationTree){
+          processVariable(((VariableDeclarationTree) element).identifier(), blockKill, blockGen);
+        }
+      }
+      //Remove from the current block
+      blockGen.removeAll(shortCircuited);
+    }
+
     private void processVariable(IdentifierTree element, Set<String> blockKill, Set<String> blockGen) {
       blockKill.add(element.name());
       blockGen.remove(element.identifier());
--- sonar-sjava-plugin/src/test/resources/NullDereferenceBeliefStyleCheck.java
+++ sonar-sjava-plugin/src/test/resources/NullDereferenceBeliefStyleCheck.java
@@ -1,298 +1,54 @@
 class A {
-
-  int f(Object p, boolean b) {
-    p.toString();
-    p == null; // Noncompliant
-    p = "";
-  }
-
-  int f(Object p, boolean b) {
-    p.toString();
-    p = "";
-    p == null; // Compliant
-  }
-
-  int f(Object p, boolean b) {
-    p = "";
-    p.toString();
-    p == null; // Noncompliant
-  }
-
-  int f(Object p, boolean b) {
-    p == null; // Compliant
-    p.toString();
-    p = "";
-  }
-
-  int f(Object p, boolean b) {
-    p == null; // Compliant
-    p = "";
-    p.toString();
-  }
-
-  int f(Object p, boolean b) {
-    p == null; // Compliant
-    p = "";
-    p.toString();
-  }
-
-  public void processTransactionTerminated(final TransactionTerminatedEvent transactionTerminatedEvent) {
-    Transaction transaction = eventTransaction;
-    String branchId = transaction.getBranchId();
-    getAsynchronousExecutor().execute(new Runnable() {
-      public void run() {
-        if(transaction == null) {} // Compliant
-      }
-    });
-  }
-
-  private void createGUI(DialogOwner dialog) {
-    assertFalse(dialog == null, "error: null dialog"); // Compliant
-    dialog.setLocation(50, 50);
-  }
-
-  int foo(Object output, boolean b) {
-    int outputCapacity = output.length - outputOffset;
-    int minOutSize = (decrypting ? (estOutSize - blockSize) : estOutSize);
-    if ((output == null) || (outputCapacity < minOutSize)) { // Noncompliant
-      throw new ShortBufferException("Output buffer must be "
-          + "(at least) " + minOutSize + " bytes long");
-    }
+  int compliant3() {
+    Object p = null;
+    p.toString(); //Compliant, FN. Our checker is a naive version, the goal is not to find these kind of obvious NP.
   }
 
   int foo(Object p, boolean b) {
-    p.toString;
-    if(p == null) {} // Noncompliant
-  }
-
-  int foo(Object p, boolean b) {
-    if(p == null) {} // Compliant
-    p.toString;
-  }
-
-  int foo(Object p, boolean b) {
-    if (cond) {
-      String s = "";
-      s.toString();
-    } else {
-      String s;
-      (s == null); // Compliant, variable declaration kill the same way as the assignmnet
-    }
-  }
-
-  int loop0(Object p, boolean b) {
-    a = "" + p.toString();
-    if(p == null){ } // Noncompliant
-  }
-
-  int foo4(Object p, boolean b) {
-    a = 1;
-    A ? B : p.toString(); // Compliant
-    p == null;
-  }
-
-  int foo3(Object p, boolean b) {
-    p.toString(); //Add the belief that p is not null
-    p = new Object();
     //...
-    if(p == null) {} // Compliant
-  }
-
-  int foo4(Object p, boolean b) {
-    String s = (p == null) ? "" : p.toString(); // Compliant
-  }
-
-  int foo4(Object p, boolean b) {
-    String s = (p == null) ? "" : p.toString(); // Compliant
-    if(p == null) {} // Compliant
-  }
-
-  int foo4(Object p, boolean b) {
-    p.toString();
-    String s = (p == null) ? "" : q.toString(); // Noncompliant
-  }
-
-  int foo41(Object p, boolean b) {
-    String s = false ? "" : p.toString(); // Compliant
-    if(p == null) {} // Compliant
-  }
-
-  int foo5(Object p, boolean b) {
-    q.fun(p.toString());
-    if(p == null) { } // Noncompliant
-  }
-
-  int assign(Object p, boolean b) {
-    foo6 = p.toString();
-    if(p == null) {} // Noncompliant
-  }
-
-  int assign2(Object p, boolean b) {
-    p = p.toString();
-    if(p == null) {} // Compliant
-  }
-
-  int foo4(Object p, boolean b) {
-    String s = a ? p.f(q.toString()) : (q == null);
-  }
-
-  int foo(boolean b) {
-    p.toString;
-    if(p == null) {} // Compliant, FN, p is a field
-  }
+    if(p == null) { } // Compliant, one path does assign new value to p.
 
 
-  //== Short circuit ===================================================
-
-  int shortcircuit01(Object p, boolean b) {
-    ((p == null)) || p.toString(); // Compliant
-    if(p == null) {} // Compliant, the pointer use has been short circuited
-  }
-
-  int shortcircuitAnd01(Object p, boolean b) {
-    p != null && p.toString(); // Compliant
-    if(p == null) {} // Compliant, the call to the fun has not been made
-  }
-
-  int shortcircuitAnd01(Object p, boolean b) {
-    p != null && a == p.toString(); // Compliant
-    if(p == null) {} // Compliant, the call to the fun has not been made
-  }
-
-  int shortcircuitAnd01(Object p, boolean b) {
-    p != null && p.toString(); // Compliant
-    if(p == null) {} // Compliant, the pointer use has been short circuited
-  }
-
-  int shortcircuit01(Object p, boolean b) {
-    p == null || p.toString(); // Compliant
-    if(p == null) {} // Compliant, the pointer use has been short circuited
-  }
-
-  int shortcircuit01(Object p, boolean b) {
-    p == null || p.toString(); // Compliant
-    p.toString();
-    if(p == null) {} // NonCompliant
-  }
-
-  int shortcircuit01(Object p, boolean b) {
-    p.size() == 0;
-    if(p == null) {} // NonCompliant
-  }
-
-  int shortcircuit01(Object p, boolean b) {
-    p.isEmpty() || b;
-    if(p == null) {} // NonCompliant
-  }
-
-  int shortcircuit01(Object p, boolean b) {
-    b == 0 || p.size() || b == 0;
-    if(p == null) {} // Compliant, FN, due to binop beeing unreliable
-  }
-
-  int shortcircuit0(Object p, boolean b) {
-    if(p == null || (a || b) || p.toString()){} // Compliant
-  }
-
-  int shortcircuit15(Object p, boolean b) {
-    if(p.equals("a") || p == null){} // Noncompliant
-  }
-
-  int shortcircuit152(Object p, boolean b) {
-    if(p.toString().equals("a") || p == null){} // Noncompliant
-  }
-
-  int shortcircuit2(Object p, boolean b) {
-    f(p == null, p.toString()); // Compliant
-  }
-
-  int shortcircuit25(Object p, boolean b) {
-    f(p.toString(), p == null); // Noncompliant
-  }
-
-  int shortcircuit3(Object p, boolean b) {
-    p == null || p.toString(); // Compliant
-  }
-
-  int shortcircuit01(Object p, boolean b) {
-    (q == a) || p == null || p.toString(); // Compliant
-  }
-
-  int shortcircuit01(Object p, boolean b) {
-    ((q == null) && p == null) || p.toString(); // Compliant
-  }
-
-  int shortcircuit01(Object p, boolean b) {
-    ((q == null) && p == null) || p.toString(); // Compliant
-    (p == null); // Compliant
-  }
-
-  int shortcircuit01(Object p, boolean b) {
-    q == null || q.toString() || (q == a) || p == null || p.toString(); // Compliant
-  }
-
-  int shortcircuit01(Object p, boolean b) {
-    (p != null && q.a == p.foo());
-    if (p == null) {} // Compliant
-  }
-
-  int shortcircuit152(Object p, boolean b) {
-    if(p.toString() || p == null){} // Noncompliant
-  }
-
-  void shortCircuit(XWindow p) {
-    (!(target instanceof Container) || p == null || p.getTarget());
-  }
-
-  int shortcircuit152(Object p, boolean b) {
-    ((p = next()) == null || p.toString()); // Compliant
-    if(p == null){} // Compliant
-  }
-
-  //==========================================================
-
-
-
-  int foo5(Object levelImpl, boolean b) {
-    if (!level.equals(levelImpl.toString())) {
-      if (levelImpl == null) { // Noncompliant
-        levelImpl = Level.DEBUG;
-        getLogger().debug("found unexpected level: " + level + ", logger: " + logger.getName() + ", msg: " + message);
-        message = level + " " + message;
-      }
+    if(a) {
+      p = new Object();
+      if(p == null){ } // Noncompliant
     }
-  }
 
-  int foo5(Object p, boolean b) {
-    q.fun(p.toString());
-    if(p == null) { } // Noncompliant
-  }
+    if(p == null) {} // Noncompliant
 
-  int foo3(Object p, boolean b) {
     p.toString(); //Add the belief that p is not null
-    p = new Object();
-    //...
-    if(p == null) {} // Compliant
+
   }
 
-  int noIf(Object p) {
-    x = p.toString();
-    boolean b = p == null; // Noncompliant
-  }
 
-  int noIf(Object p) {
-    p.toString();
-    boolean b = p == null; // Noncompliant
-  }
-
-  int noIf2(Object p) {
-    boolean b = p == null; // Compliant, forward analysis
-    p.toString();
+  int compliant(Object p, boolean b) {
+    p.toString(); //Compliant
+    if(p != null){ } // Noncompliant
   }
 
   int compliant(Object p, boolean b) {
-    if(p == null){ } //Compliant
-    p.toString(); //Compliant, even though we have not assign p
+    p.toString(); //Compliant
+    if(null != p){ } // Noncompliant
+  }
+
+  int compliant(Object p, boolean b) {
+    if(null != p){ } // Noncompliant
+    p.toString(); //Compliant
+  }
+
+  int compliant(Object p, boolean b) {
+    p.toString(); //Compliant
+    if(p != p){ } // Compliant
+  }
+
+  int compliant(Object p, boolean b) {
+    p.toString(); //Compliant
+    if(null == p){ } // Noncompliant
+  }
+
+  int compliant(Object p, boolean b) {
+    if(p == null){ } // Noncompliant
+    p.toString(); // Compliant
   }
 
   int compliant2(Object p, boolean b) {
@@ -322,10 +78,22 @@ class A {
   }
 
   int foo2(Object p, boolean b) {
-    p = "";
+    p = new Object();
     p.toString(); //Add the belief that p is not null
     //...
     if(p == null) {} // Noncompliant
+  }
+
+  int foo3(Object p, boolean b) {
+    p.toString(); //Add the belief that p is not null
+    p = new Object();
+    //...
+    if(p == null) {} // Compliant
+  }
+
+  int foo4(Object p, boolean b) {
+    //...
+    if(p == null || p.toString().equals("a")){} // Compliant
   }
 
   int foo5(Object p, boolean b) {
@@ -336,40 +104,9 @@ class A {
 
   //== LOOP ===================================================
 
-  int loop0(Object p, boolean b) {
-    for(int i = 0; i < 10; i ++){
-      p.toString();
-    }
-    if(p == null){ } // Compliant, a path (entry) have not used p
-  }
-
-  int loop0(Object p, boolean b) {
-    for(int i = 0; i < p.toString(); i ++){
-
-    }
-    if(p == null){ } // Compliant, FN, the check is in a native node
-  }
-
-  int loop0(Object p, boolean b) {
-    p.toString();
-    for(int i = 0; i < 10; i ++){
-      b = false;
-    }
-    if(p == null){ } // Noncompliant
-  }
-
   int loop(Object p, boolean b) {
-    b = false;
     for(int i = 0; i < 10; i ++){
-      if(p == null){ } // Compliant
-      p.toString();
-    }
-  }
-
-  int loop1(Object p, boolean b) {
-    b = false;
-    for(int i = 0; i < 10; i ++){
-      if(p == null){ } // Compliant
+      if(p == null){ } // Noncompliant
       p.toString();
     }
   }
@@ -394,9 +131,11 @@ class A {
   int loop4(Object p, boolean b) {
     if(p.toString().equals("a")) {
       while (p == null) { // Noncompliant
+
       }
     }
   }
+
 
   int loop5(Object p, boolean b) {
     p.toString();
@@ -418,27 +157,22 @@ class A {
     }
   }
 
-  int loop0(Object p, boolean b) {
-    for(int i = 0; i < p.size(); i ++){
-      a = 1;
+  private Map.Entry<K,V> lowestEntry() {
+    for (; ; ) {
+      ConcurrentSkipListMap.Node<K, V> n = loNode();
+      if (!isBeforeEnd(n))
+        return null;
+      Map.Entry<K, V> e = n.createSnapshot();
+      if (e != null)
+        return e;
     }
-    if(p == null){ } // Compliant, FN due to for loop head
   }
 
-  public int assignInsideHeader(Component a, Component b) {
-    a.getParent();
-    for (int i = 0; a != null; a = a.getParent()) {
-      b = 1;
-    }
-    if (a == null) { } // Compliant, a is assigned inside the header (that is a native)
-  }
+  private String shadowString(String s) {
+    s.toString();
 
-  int newPInLoop(Object p, boolean b) {
-    p.toString();
-    while (cond) {
-      Object p = getP();
-      if(p == null){ } // Compliant, p that is used is not the same as the one that is checked
-    }
+    String s = "";
+    if(s == null){ } // Compliant
   }
 
   //== Exception =======================================
@@ -478,21 +212,7 @@ class A {
     try{
       doSomething();
     } catch(Exception e) {
-      doSomething();
-    } finally {
       b = new Object();
-    }
-
-    if(b == null){ } // Compliant, the finally path reasign the value
-  }
-
-  void foo(boolean a, Object b) {
-    try{
-      b.toString();
-      if(b == null){ } // Compliant, might be considered as a FN... (due to the fact that we don't gen in try block)
-    } catch(Exception e) {
-      if(b == null){ } // Compliant, the NP might have been catched.
-      doSomething();
     }
 
     if(b == null){ } // Compliant
@@ -500,99 +220,73 @@ class A {
 
   void foo(boolean a, Object b) {
     b.toString();
+
     try{
-      if(b == null){ } // Noncompliant
-    } catch(Exception e) {
-      if(b == null){ } // Noncompliant
       doSomething();
+    } catch(Exception e) {
+      doSomething();
+    } finally {
+      b = new Object();
     }
 
-    if(b == null){ } // Noncompliant
+    if(b == null){ } // Compliant
   }
 
   void doSomething() { }
 
-  //== Other =======================================
-
-  private String shadowString(String s) {
-    s.toString();
-
-    String s = "";
-    if(s == null){ } // Compliant
-  }
-
-  void inReturn(String p) {
-    p.toString();
-    return p == null; // Noncompliant
-  }
-
-  void nullObject() {
-    A a = new A();
-
-    a.equals("a");
-
-    if(a == null){ // Noncompliant
-
-    }
-  }
-
-  void nullObject() {
-    A a = new A();
-
-    a.s;
-
-    if(a == null){ // Noncompliant
-
-    }
-  }
-
-  void nullArrayAcess(int[] p) {
-    p[0] = 1;
-    if(p == null){ // Compliant, FN array not supported
-
-    }
-  }
-
-  void nullArrayAcess(int[] p) {
-    int l = p.length;
-    if(p == null) { // Noncompliant
-      //p.length is a seen as a member select in Slang
-    }
-  }
 
   private static class A {
     String s = "";
 
     void foo() {
-      if(s == null){ // Compliant
+      if(s == null){ //
         s = "a";
       }
       s.toString();
       changeS(); // An other method can change the value of s!
 
-      if(s == null) { // Compliant, s is a field
-
+      if(s == null) { // Compliant, s can change in the function call
       }
     }
 
-    String getS() {
-      return "s";
-    }
-
     void changeS() {
-      s = "";
+      s = null;
     }
+
   }
 
-  //== In code =======================================
-
-  public boolean accept(long value) {
-    return ((valids == null) || (valids.contains(value))) && ((invalids == null) || (!invalids.contains(value))); // Compliant
+  // BACKWARD =========================================
+  int foo(Object p, boolean b) {
+    if(p == null) { // Noncompliant
+      p.toString(); //Add the belief that p is not null
+      p = new Object();
+    }
+    p.toString(); //Add the belief that p is not null
   }
 
-  public boolean equals(Object o) {
-    return a ? Objects.equals(dateTimeFormatter.locale(), that.dateTimeFormatter.locale())
-        : dateTimeFormatter == null; // Compliant
+  int foo(Object p, boolean b) {
+    if(p == null) { // Compliant, one path re-assign p
+      p = new Object();
+      p.toString(); //Add the belief that p is not null
+    }
+    p.toString(); //Add the belief that p is not null
   }
 
+  int foo(Object p, boolean b) {
+    p = new Object();
+    if(p == null) { } // Noncompliant
+    p.toString(); //Add the belief that p is not null
+  }
+
+  int compliant(Object p, boolean b) {
+    if(p == null){ } // Noncompliant
+    p.toString(); //due to backward analysis
+  }
+
+  int compliant2(Object p, boolean b) {
+    if (p == null) {//Compliant, one path has assigned p
+      p = new Object();
+    }
+    p.toString(); //Compliant
+  }
 }
